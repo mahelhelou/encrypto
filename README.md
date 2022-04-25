836f825c71db5592531ef1c848e1d2071a57099e
Mahmoud El Helou <mahelhelou@gmail.com>
2022-04-25 13:28:06 +0300

    Finish the functionality

---
+++ README.md
@@ -0,0 +1,10 @@
+# Documentation
+
+## About Encrypto
+
+## How it Works?
+
+1. Type the string you want to encrypt to decrypt in the input field.
+2. Select the conversion option, encrypt or decrypt.
+3. Click the button named "Submit".
+4. A generated text below the input will shown, have fun with it!
--- app.js
+++ app.js
@@ -1,14 +1,67 @@
 // UI elements
 const encryptionForm = document.querySelector('.encryption-form')
 const encryptionInput = document.querySelector('.encryption-input')
-const encryptionOption = document.querySelector('.encryption-options').value
 const encryptionBtn = document.querySelector('.encryption-button')
 const encryptionOutput = document.querySelector('.encryption-output')
 
-console.log(encryptionOption)
+// Encrypt the string
+const cipher = salt => {
+	const textToChars = text => text.split('').map(c => c.charCodeAt(0))
+	const byteHex = n => ('0' + Number(n).toString(16)).substr(-2)
+	const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code)
 
-encryptionBtn.addEventListener('click', function (e) {
+	return text => text.split('').map(textToChars).map(applySaltToChar).map(byteHex).join('')
+}
+
+// Decrypt the string
+const decipher = salt => {
+	const textToChars = text => text.split('').map(c => c.charCodeAt(0))
+	const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code)
+	return encoded =>
+		encoded
+			.match(/.{1,2}/g)
+			.map(hex => parseInt(hex, 16))
+			.map(applySaltToChar)
+			.map(charCode => String.fromCharCode(charCode))
+			.join('')
+}
+
+/**
+ * On submitting the form, do the following:
+ * 1. READ the input string
+ * 2. READ the select option value
+ * 3. CONVERT the string into encrypted ot decrypted version
+ * 4. PRINT the value (Converted string) below the input
+ */
+encryptionForm.addEventListener('submit', e => {
 	e.preventDefault()
 
-	alert('Hello!')
+	const encryptionSelect = document.querySelector('.encryption-options')
+	let encryptionOption = encryptionSelect.value
+
+	encryptionSelect.addEventListener('change', () => {
+		encryptionOption = encryptionSelect.options[encryptionSelect.selectedIndex].value
+	})
+
+	if (encryptionOption === 'encrypt') {
+		// To create a cipher
+		const myCipher = cipher('mySecretSalt')
+
+		// Then cipher any text:
+		const encryptedText = myCipher(encryptionInput.value)
+
+		encryptionOutput.insertAdjacentHTML('beforeend', `<p>${encryptedText}</p>`)
+	}
+
+	if (encryptionOption === 'decrypt') {
+		// To decipher, you need to create a decipher and use it:
+		const myDecipher = decipher('mySecretSalt')
+
+		// Decrypt any string
+		const decryptedString = myDecipher(encryptionInput.value)
+
+		console.log(decryptedString)
+
+		encryptionOutput.insertAdjacentHTML('beforeend', `<p>${decryptedString}</p>`)
+	}
 })
